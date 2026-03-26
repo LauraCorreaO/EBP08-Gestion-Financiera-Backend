bac78b44e2474258110f7c02ea0abbb4738572af
miguemcu <miguemej07@gmail.com>
2026-03-26 18:23:10 -0500

    Creación proyecto base

---
+++ gestion-financiera-backend/src/main/java/com/ebp08/gestion_financiera_backend/GestionFinancieraBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ebp08.gestion_financiera_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionFinancieraBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionFinancieraBackendApplication.class, args);
+	}
+
+}
