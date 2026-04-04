8eb7607680e29b0c2bd887c678a04675d979d07c
Simón Montoya <s.montoya1@udea.edu.co>
2026-04-04 12:18:58 -0500

    pequeños cambios

--- gestion-financiera-backend/src/main/java/com/ebp08/gestion_financiera_backend/repository/PresupuestoRepository.java
+++ gestion-financiera-backend/src/main/java/com/ebp08/gestion_financiera_backend/repository/PresupuestoRepository.java
@@ -18,14 +18,16 @@ public interface PresupuestoRepository extends JpaRepository<Presupuesto, Long> 
        "AND MONTH(p.fechaLimite) = MONTH(CURRENT_DATE) " +// Filtra por mes actual
        "AND YEAR(p.fechaLimite) = YEAR(CURRENT_DATE)")// Filtra por año actual
 
-    //
-    Optional<Presupuesto> findByUsuarioIdAndMesActual(@Param("usuarioId") Long usuarioId); //
+    
+    Optional<Presupuesto> findByUsuarioIdAndMesActual(@Param("usuarioId") Long usuarioId); 
 
     // Consulta personalizada para encontrar un presupuesto por categoría en el mes actual
     @Query("SELECT p FROM Presupuesto p WHERE p.usuario.id = :usuarioId " + 
        "AND p.categoria.id = :categoriaId " + // Filtra por categoría específica, ":" es un placeholder para el parámetro que se pasará a la consulta
        "AND MONTH(p.fechaLimite) = MONTH(CURRENT_DATE) " +
        "AND YEAR(p.fechaLimite) = YEAR(CURRENT_DATE)")
+
+
     Optional<Presupuesto> findByUsuarioIdAndCategoriaIdAndMesActual( 
         @Param("usuarioId") Long usuarioId, 
         @Param("categoriaId") Long categoriaId); // @param para pasar el id de la categoría como parámetro a la consulta personalizada
