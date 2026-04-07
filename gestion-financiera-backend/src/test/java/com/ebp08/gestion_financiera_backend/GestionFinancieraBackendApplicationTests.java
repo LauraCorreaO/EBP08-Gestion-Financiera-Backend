9bf3fed2304068479c615846c5c4d2e7874a2ed9
LCORREAO <LCORREAO@proteccion.com.co>
2026-04-07 13:59:37 -0500

    Agregar TestProperty

--- gestion-financiera-backend/src/test/java/com/ebp08/gestion_financiera_backend/GestionFinancieraBackendApplicationTests.java
+++ gestion-financiera-backend/src/test/java/com/ebp08/gestion_financiera_backend/GestionFinancieraBackendApplicationTests.java
@@ -2,8 +2,10 @@ package com.ebp08.gestion_financiera_backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application.properties")
 class GestionFinancieraBackendApplicationTests {
 
 	@Test
